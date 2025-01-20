cec9aa923a978c3cdad2b22b6d4f53cac032924e
Nileshh1010 <nileshkrsingh1003@gmail.com>
2025-01-21 01:01:54 +0530

    Added backend

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from db.database import init_db
+from routes.auth_routes import auth_routes
+from routes.task_routes import task_routes
+from flask_cors import CORS
+app = Flask(__name__)
+cors = CORS(app, resources={
+    r"/*": {
+        "origins": ["https://task-manager-frontend-eight-teal.vercel.app"],  # Frontend URL
+        # Allow sending cookies
+        "supports_credentials": True 
+        }
+})
+
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Initialize Database
+init_db()
+
+# Register Blueprints
+app.register_blueprint(auth_routes, url_prefix='/auth')
+app.register_blueprint(task_routes, url_prefix='/tasks')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000)
